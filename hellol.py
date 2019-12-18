694a9e7a65331d1a63c80e4f9b1775d51b0442e0
pualahn <jyahn0623@gmail.com>
2019-12-18 10:45:02 +0900

    hellol.py modified

--- hellol.py
+++ hellol.py
@@ -1,39 +1,13 @@
-from collections import Counter, OrderedDict
-def solution(genres, plays):
-    answer = []
-    _dict = {}
-    _number = { k : [] for k in set(genres) }
-    rank = []
-    i = 0
-    # 통합 데이터
-    for k, v in zip(genres, plays):
-        _number[k].append((i, v))
-        try:
-            _dict[k] += v
-        except:
-            _dict[k] = 0
-            _dict[k] += v
-            rank.append(k)
-        i+=1
-    
-    # 순위 
-    for i, k in enumerate(rank):
-        for j, v in enumerate(rank):
-            if _dict[k] > _dict[v]:
-                tmp = rank[i]
-                rank[i] = rank[j]
-                rank[j] = tmp
-    
-    # 베스트 앨범 출시
-    for i in rank:
-        __rank = []
-        _list = _number[i]
-        c = OrderedDict(Counter(dict(_list)).most_common())
-        j = 0
-        for v in c:
-            if j < 2:
-                answer.append(v)
-                j+=1
-    return answer
 
-solution(['a', 'b', 'a', 'b', 'a', 'c'], [200, 300, 400, 40, 200, 1000])
+a = 1
+
+mast = 1 << 3
+
+# 1000 
+#  1이 1인가?
+
+def updateBit(num, i, val):
+    print( num | val << i )
+    print( num & ~( 1<<i ) | (val << i))
+
+updateBit(169, 3, 0)
